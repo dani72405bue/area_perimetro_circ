a5cd24ea6ab025451650dfbea9f82c8db16018e8
dani72405bue <danielbueno@colegioguanenta.edu.co>
2026-01-30 06:58:54 -0500

    se actualizo el readme con analisis y diseño

---
+++ area_perimetro_circ.py
@@ -0,0 +1,28 @@
+# programa para calcular el area y el perimetro de un circulo
+
+# libreria
+import math 
+# ------------
+# input 
+# ------------
+
+print ("------------")
+print ("area y perimetro del circulo")
+print ("------------")
+
+r= input ("digite el valor del radio ")
+r= int(r)
+
+# --------
+# processing 
+# --------
+a= math.pi*r**2
+p= 2*math.pi*r
+
+# output
+print ("-------------")
+print ("---resultados---")
+print ("-------------")
+print (" El área del circulo es: " + str(a))
+print ("El perimetro del circulo es: " + str(p))
+print ("---------------")
